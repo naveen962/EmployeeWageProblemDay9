6205ec29eccbd192d3b92555c00c12be414dd075
naveen962 <mrn28897@gmail.com>
2021-09-28 05:08:21 -0700

    program calculate the multiple companywages program is added

---
+++ EmployeeWageUC7/EmployeeWageUC7/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmployeeWageUC7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeWageUC7.UseCase7 uc = new UseCase7();
+            uc.ComputeEmpWage("BridgeLabz", 20, 10, 50);
+            uc.ComputeEmpWage("JIO", 10, 20, 100);
+        }
+    }
+}
---
+++ EmployeeWageUC7/EmployeeWageUC7/UseCase7.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageUC7
+{
+    class UseCase7
+    {
+        public void ComputeEmpWage(string Company ,int EMPWAGE_PER_HOUR, int Num_Working_of_days, int max_work_hrs_month)
+
+        {
+           
+            //variables
+            int EmpHours = 0;
+            int Empwage = 0;
+            int Totalwages = 0;
+            int Totalworkhrs = 0;
+            int totalworkingdays = 0;
+
+
+            //computation
+            while (Totalworkhrs <= max_work_hrs_month && totalworkingdays < Num_Working_of_days)
+            {
+                Random ran = new Random();
+                int Empcheck = ran.Next(0, 3);
+                switch (Empcheck)
+                {
+                    case 1:
+                        Console.WriteLine("PART TIME");
+                        EmpHours = 4;
+                        break;
+
+                    case 2:
+                        Console.WriteLine("FULL Time");
+                        EmpHours = 8;
+                        break;
+                    default:
+                        Console.WriteLine("ABSENT");
+                        EmpHours = 0;
+                        break;
+
+                }
+                totalworkingdays++;
+                Totalworkhrs += EmpHours;
+
+                Empwage = Totalworkhrs * EMPWAGE_PER_HOUR;
+                Console.WriteLine("TOTAL EMPLOYEE WAGE FOR COMPANY "+Company+"is :" + Empwage);
+                Totalwages += Empwage;
+               
+
+            }
+            Console.WriteLine("TOTAL WAGES OF COMPANY =" + Totalwages);
+        }
+    }
+}
