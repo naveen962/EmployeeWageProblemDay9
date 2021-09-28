4f9442ed9813d98da1b53e3ee89b05130db3a1e2
naveen962 <mrn28897@gmail.com>
2021-09-28 04:35:43 -0700

    Welcome to Employeewage computation is added

---
+++ EmployeeWageStart/EmployeeWageStart/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EmployeeWageUC1
+{
+    class Program
+    {
+
+        public void greet()
+        {
+            Console.WriteLine("Welcome To EmployeeWage Computation");
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.greet();
+        }
+    }
+}
